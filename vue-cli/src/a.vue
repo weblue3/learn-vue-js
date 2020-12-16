7599446c087d5a723daccbc363a37775a7a24df7
weblue3 <weblue2@gmail.com>
2020-12-16 15:39:31 +0900

    vue cli 프로젝트 생성

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    addNum: function () {},
+  },
+};
+</script>
+
+<style>
+/* CSS */
+</style>
